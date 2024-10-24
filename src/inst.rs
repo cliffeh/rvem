f65390270660f5654d2d0cb9987c2236eacec4a9
Cliff Snyder <cliff.snyder@gmail.com>
2024-10-24 11:45:55 -0400

    more instruction formatting

--- src/inst.rs
+++ src/inst.rs
@@ -63,29 +63,11 @@ impl Inst {
 }
 
 impl std::fmt::Display for Inst {
+    // NB this is a bit of a hack, but we're going to repurpose precision
+    // to pass in the instruction's address in memory
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Inst::ADDI { rd, rs1, imm } => {
-                if *rs1 == Reg::zero {
-                    write!(f, "li {}, {}", rd, sext(*imm, 12) as i32)
-                } else {
-                    write!(f, "addi {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)?;
-                    Ok(())
-                }
-            }
-
-            Inst::ORI { rd, rs1, imm } => {
-                write!(f, "or {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)
-            }
-
-            Inst::AUIPC { rd, imm } => {
-                write!(f, "auipc {}, 0x{:x}", rd, *imm)
-            }
-
-            Inst::LW { rd, rs1, imm } => {
-                write!(f, "lw {}, {}({})", rd, sext(*imm, 12) as i32, rs1)
-            }
-
+            /* B-Type */
             Inst::BEQ { rs1, rs2, imm } => {
                 let addr = if let Some(pc) = f.precision() {
                     format!("{:x}", pc as i32 + sext(*imm, 12) as i32)
@@ -95,10 +77,107 @@ impl std::fmt::Display for Inst {
                 write!(f, "beq {}, {}, {addr}", rs1, rs2)
             }
 
-            Inst::ADD { rd, rs1, rs2 } => {
-                write!(f, "add {}, {}, {}", rd, rs1, rs2)
+            Inst::BNE { rs1, rs2, imm } => {
+                let addr = if let Some(pc) = f.precision() {
+                    format!("{:x}", pc as i32 + sext(*imm, 12) as i32)
+                } else {
+                    format!("PC+{}", sext(*imm, 12) as i32)
+                };
+                write!(f, "bne {}, {}, {addr}", rs1, rs2)
             }
 
+            Inst::BLT { rs1, rs2, imm } => {
+                let addr = if let Some(pc) = f.precision() {
+                    format!("{:x}", pc as i32 + sext(*imm, 12) as i32)
+                } else {
+                    format!("PC+{}", sext(*imm, 12) as i32)
+                };
+                write!(f, "blt {}, {}, {addr}", rs1, rs2)
+            }
+
+            Inst::BGE { rs1, rs2, imm } => {
+                let addr = if let Some(pc) = f.precision() {
+                    format!("{:x}", pc as i32 + sext(*imm, 12) as i32)
+                } else {
+                    format!("PC+{}", sext(*imm, 12) as i32)
+                };
+                write!(f, "bge {}, {}, {addr}", rs1, rs2)
+            }
+
+            Inst::BLTU { rs1, rs2, imm } => {
+                let addr = if let Some(pc) = f.precision() {
+                    format!("{:x}", pc as i32 + sext(*imm, 12) as i32)
+                } else {
+                    format!("PC+{}", sext(*imm, 12) as i32)
+                };
+                write!(f, "bltu {}, {}, {addr}", rs1, rs2)
+            }
+
+            Inst::BGEU { rs1, rs2, imm } => {
+                let addr = if let Some(pc) = f.precision() {
+                    format!("{:x}", pc as i32 + sext(*imm, 12) as i32)
+                } else {
+                    format!("PC+{}", sext(*imm, 12) as i32)
+                };
+                write!(f, "bgeu {}, {}, {addr}", rs1, rs2)
+            }
+
+            /* I-Type */
+
+            // integer operations
+
+            Inst::ADDI { rd, rs1, imm } => {
+                if *rs1 == Reg::zero {
+                    write!(f, "li {}, {}", rd, sext(*imm, 12) as i32)
+                } else {
+                    write!(f, "addi {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)?;
+                    Ok(())
+                }
+            }
+
+            Inst::ANDI { rd, rs1, imm } => {
+                write!(f, "andi {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)
+            }
+
+            Inst::ORI { rd, rs1, imm } => {
+                write!(f, "ori {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)
+            }
+
+            Inst::SLTI { rd, rs1, imm } => {
+                write!(f, "slti {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)
+            }
+
+            Inst::SLTIU { rd, rs1, imm } => {
+                write!(f, "sltiu {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)
+            }
+
+            Inst::XORI { rd, rs1, imm } => {
+                write!(f, "xori {}, {}, {}", rd, rs1, sext(*imm, 12) as i32)
+            }
+
+            // loads
+
+            Inst::LB { rd, rs1, imm } => {
+                write!(f, "lb {}, {}({})", rd, sext(*imm, 12) as i32, rs1)
+            }
+
+            Inst::LH { rd, rs1, imm } => {
+                write!(f, "lh {}, {}({})", rd, sext(*imm, 12) as i32, rs1)
+            }
+
+            Inst::LW { rd, rs1, imm } => {
+                write!(f, "lw {}, {}({})", rd, sext(*imm, 12) as i32, rs1)
+            }
+
+            Inst::LBU { rd, rs1, imm } => {
+                write!(f, "lbu {}, {}({})", rd, sext(*imm, 12) as i32, rs1)
+            }
+
+            Inst::LHU { rd, rs1, imm } => {
+                write!(f, "lhu {}, {}({})", rd, sext(*imm, 12) as i32, rs1)
+            }
+
+            /* J-Type */
             Inst::JAL { rd, imm } => {
                 if let Some(pc) = f.precision() {
                     write!(f, "j {:x}", (pc as i32 + sext(*imm, 20) as i32))
@@ -107,6 +186,37 @@ impl std::fmt::Display for Inst {
                 }
             }
 
+            /* R-Type */
+            Inst::ADD { rd, rs1, rs2 } => {
+                write!(f, "add {}, {}, {}", rd, rs1, rs2)
+            }
+
+            Inst::AND { rd, rs1, rs2 } => {
+                write!(f, "and {}, {}, {}", rd, rs1, rs2)
+            }
+
+            Inst::OR { rd, rs1, rs2 } => {
+                write!(f, "or {}, {}, {}", rd, rs1, rs2)
+            }
+
+            Inst::SUB { rd, rs1, rs2 } => {
+                write!(f, "sub {}, {}, {}", rd, rs1, rs2)
+            }
+
+            Inst::XOR { rd, rs1, rs2 } => {
+                write!(f, "xor {}, {}, {}", rd, rs1, rs2)
+            }
+
+            /* U-Type */
+            Inst::AUIPC { rd, imm } => {
+                write!(f, "auipc {}, 0x{:x}", rd, *imm)
+            }
+
+            Inst::LUI { rd, imm } => {
+                write!(f, "lui {}, 0x{:x}", rd, *imm)
+            }
+
+            /* syscalls */
             Inst::ECALL => write!(f, "ecall"),
             _ => {
                 // TODO implement Diplay for all the rest of the instruction types
